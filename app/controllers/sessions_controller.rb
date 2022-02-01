7d4428d9d8e17c2cc14317a84a5d2004035cc29d
Jose Algorta <jpalgort@gmail.com>
2022-02-01 11:45:42 -0300

    Add login functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,23 @@
 class SessionsController < ApplicationController
-  def index
-  end
-
   def new
   end
 
   def create
+    user = User.find_by username: params[:session][:username]
+
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Glad to have you back!'
+      redirect_to chatroom_path
+    else
+      flash.now[:error] = 'Invalid credentials'
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = 'See you soon!'
+    redirect_to root_path
   end
 end
